d08da609859f9cdb21e0575adf6c6fe791051f2b
Jan Vlcek <vlki@vlki.cz>
2026-01-03 14:13:43 +0100

    ostatni sekce

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -224,6 +224,242 @@ export default function Home() {
             poplatku.
           </p>
         </div>
+
+        <SectionsSeparator />
+
+        <div className="mx-auto max-w-[1144px] px-[22px] pt-[85px] pb-[100px] sm:px-[52px]">
+          <h2 className="font-rubik text-[40px]/[1.2] font-black text-[#934541] uppercase">
+            Jak přistupujeme k historii
+          </h2>
+          <p className="mt-[16px] text-[24px]/[1.2]">
+            Sametová léta nejsou přehlídkou nostalgie, ani snahou vyřknout jasný
+            soud nad problematikou komunistického režimu v Československu.
+            Chceme vykreslit širší společenské trendy, které provázely přelom
+            80. a 90. let, prostřednictvím každodenních životů obyčejných lidí.
+            Klíčovými tématy hry jsou kdo získá byt, kdo se dostane na školu,
+            komu se rozpadne vztah nebo kdo jak vyjde z přelomové doby konce
+            osmdesátých let. Součástí hry jsou i další společenská témata doby
+            jako postavení Romů v československé společnosti, ekologická hnutí
+            nebo vývoj drogové scény. Všechny postavy ve hře jsou fiktivní – a
+            jejich příběhy se mohou rozvinout mnoha různými směry.
+          </p>
+          <p className="mt-[16px] text-[24px]/[1.2]">
+            V rámci hry budete pracovat s informacemi z reálných historických
+            materiálů. Mnohé z nich, jak oficiální (např. státní televize), tak
+            neoficiální, mohou být nespolehlivé. Stejně jako si ve svém životě
+            každý vybíráme, které informace jsou podle nás důvěryhodné, a jak je
+            interpretovat, bude na hrajících, aby je interpretovali v rolích
+            svých postav.
+          </p>
+        </div>
+
+        <SectionsSeparator />
+
+        <div className="mx-auto max-w-[1144px] px-[22px] pt-[85px] pb-[100px] sm:px-[52px]">
+          <h2 className="font-rubik text-[40px]/[1.2] font-black text-[#934541] uppercase">
+            Paralelní osudy
+          </h2>
+          <p className="mt-[16px] text-[24px]/[1.2]">
+            Na dovolenou přijede pokaždé 22 postav, jedna parta z jedné fabriky.
+            Ta prožije své životy, volby, radosti i trápení. Hra samotná ale
+            běží paralelně pro více poboček patřících pod podnik: na podzim 2026
+            budou dva běhy po dvou pobočkách. Většinu hry spolu jako postavy
+            nebudete interagovat – jsou to cizí lidé na podnikové dovolené, se
+            kterými je možné prohodit pár slov ve frontě na oběd, ale těžiště
+            vaší hry v nich neleží.
+          </p>
+          <p className="mt-[16px] text-[24px]/[1.2]">
+            Pro vás jako pro hráče ale zajímaví jsou. Všechny pobočky se totiž
+            ve skutečnosti skládají ze stejných postav. Ve specifických chvílích
+            se potkáte se svým protějškem z jiné pobočky. Budete se moci
+            poradit, reflektovat spolu své osudy a nejspíš se i podivit, jak
+            odlišné nebo naopak podobné životy vy nebo vaši blízcí teď na
+            základě svých rozhodnutí žijete. Jeden z vás možná bude ředitel
+            podniku a druhý kotelník, jeden šťastně zamilovaný a druhý trpce
+            osamělý. Úvodní pozici máte stejnou, ale toto je jeden z nástrojů,
+            kterými zdůrazňujeme téma zlomové doby, která zpřeházela zaběhnuté
+            vztahy a pořádky.
+          </p>
+        </div>
+
+        <SectionsSeparator />
+
+        <div className="mx-auto max-w-[1144px] px-[22px] pt-[85px] pb-[100px] sm:px-[52px]">
+          <h2 className="font-rubik text-[40px]/[1.2] font-black text-[#934541] uppercase">
+            Co je to larp?
+          </h2>
+          <p className="mt-[16px] text-[24px]/[1.2]">
+            Sametová léta jsou larp, tj. hraní rolí naživo (z anglického
+            “live-action roleplaying”. Jedná se několikadenní hru, během které
+            skupina hráčů zaujme role předpřipravených postav dodaných
+            organizátory (určujících jejich minulost, rodinné vztahy, ale třeba
+            i ambice a strachy) a společně nechají ožít příběh běžných lidí na
+            dovolené na pozadí 80. – 90. let minulého století v Československu.
+            Výchozí body postav jsou dané a každá z nich má připravené od
+            organizátorů obsah k řešení, ale veškeré volby, průběh a to, jak
+            příběh dané postavy nakonec dopadne, je plně v rukách hráčů.
+          </p>
+          <p className="mt-[16px] text-[24px]/[1.2]">
+            Na hru se bude možné přihlásit pomocí online formuláře, ve kterém
+            vyplníš své preference o tom, co na hře chceš a nechceš zažít.
+            Hráčům vybraným na hru pak podle jejich odpovědí přidělíme postavu,
+            kterou budou během hry ztvárňovat. Detailní informace o postavě
+            dostaneš několik týdnů před začátkem hry.
+          </p>
+          <p className="mt-[16px] text-[24px]/[1.2]">
+            Během hry pak v roli své postavy budeš uvádět v život její činy,
+            rozhodnutí, interagovat s ostatními postavami a užívat si radosti i
+            strasti podnikové dovolené.{" "}
+            <strong>Není potřeba ovládat herectví, ani mít trému.</strong> Hra
+            nemá žádné diváky, vítěze a poražené. Cílem také není hru vyhrát,
+            ale zažít zajímavé situace, zábavu a trochu té lidskosti. Jako hráče
+            tě nečeká žádné nepohodlí nad rámec “bydlím v chatce”, nebudeš se s
+            jinými hráči bít ani střílet a hra ani nepracuje s žádným skutečným
+            intimním kontaktem mezi hráči.
+          </p>
+          <p className="mt-[16px] text-[24px]/[1.2]">
+            Platí, že <strong>hráč není postava</strong>. Např. hráč Jan Novák,
+            voják, hraje postavu Tomáše Černého, oblíbeného obchodního náměstka
+            podniku. Pokud je Tomáš na tvoji postavu celou hru nepříjemný,
+            neznamená to, že je takový Jan. A platí to i obráceně – pokud Tomáš
+            a tvoje postava prožívali během hry romantický vztah, neznamená to,
+            že tento vztah mezi Janem a tebou bude pokračovat i po hře.
+          </p>
+        </div>
+
+        <SectionsSeparator />
+
+        <div className="mx-auto max-w-[1144px] px-[22px] pt-[85px] pb-[100px] sm:px-[52px]">
+          <h2 className="font-rubik text-[40px]/[1.2] font-black text-[#934541] uppercase">
+            Bezpečnost na hře
+          </h2>
+          <p className="mt-[16px] text-[24px]/[1.2]">
+            Nevyhnutelnou součástí hry je, že postavy budou ve svých životech
+            prožívat mnohdy náročné nebo nepříjemné situace. Proto je pro nás
+            zároveň prioritou, abychom udělali maximum pro duševní i fyzické
+            bezpečí hráčstva. Kromě těžkých životních situací se hra dotýká i
+            témat jako postavení Romů v československé společnosti, ekologická
+            hnutí nebo vývoj drogové scény. Sexuální násilí není tématem hry.
+          </p>
+          <p className="mt-[16px] text-[24px]/[1.2]">
+            Před začátkem hry proběhnou povinné workshopy, kde si společně
+            probereme, jak hru hrát a co nejvíce si ji užít. Stejně tak si na
+            nich ukážeme, že každý má v případě potřeby možnost herní situaci
+            kdykoliv opustit, a jak to udělat. Platí, že účast na hře je po
+            celou její dobu dobrovolná, a že zdraví má vždy prioritu před hrou.
+          </p>
+          <p className="mt-[16px] text-[24px]/[1.2]">
+            Po celou dobu akce bude hráčstvu k dispozici
+            organizátor/organizátorka na konzultace či krizovou intervenci a
+            zdravotník/zdravotnice. Během hry je povolena konzumace alkoholu v
+            rozumné míře, tedy aby neovlivnila hru. Obsluha baru se v případě
+            potřeby může kdykoliv rozhodnout někomu alkohol nenalít.
+          </p>
+        </div>
+
+        <SectionsSeparator />
+
+        <div className="mx-auto max-w-[1144px] px-[22px] pt-[85px] pb-[100px] sm:px-[52px]">
+          <h2 className="font-rubik text-[40px]/[1.2] font-black text-[#934541] uppercase">
+            Autorstvo
+          </h2>
+
+          <div className="mt-[24px] flex flex-col gap-[24px] sm:flex-row sm:gap-[60px]">
+            <ul className="flex flex-col gap-[24px] sm:grow-1 sm:basis-0 sm:gap-[36px]">
+              <li>
+                <h3 className="font-rubik text-[24px]/[1.2] font-black text-[#934541] uppercase">
+                  David František Wagner
+                </h3>
+                <p className="mt-[8px] text-[24px]/[1.2]">
+                  První larp uvedl v 16, tento chystá v 37. Někde mezitím se
+                  vyučil učitelem a historikem, pracuje jako politický poradce a
+                  manažer a snaží se být dobrým vedoucím projektu Sametová léta.
+                  Upřímně věří, že larp je smysluplný nástroj vyprávění o dané
+                  době.
+                </p>
+              </li>
+              <li>
+                <h3 className="font-rubik text-[24px]/[1.2] font-black text-[#934541] uppercase">
+                  Lucie Chlumská
+                </h3>
+                <p className="mt-[8px] text-[24px]/[1.2]">
+                  Kdysi byla hrdou členkou Jisker, pak přišla revoluce a
+                  skončila v Rollingu. Vystudovala psychologii a speciální
+                  pedagogiku, což se jí hodí při práci personalistky a při
+                  organizování larpů (např. Legie: Sibiřský příběh, Národ sobě,
+                  Příliš krátká neděle). Na tomto projektu může konečně využít
+                  svou lásku k lampionovým průvodům a nenávist k organizovanému
+                  programu na rekreačních akcích.
+                </p>
+              </li>
+              <li>
+                <h3 className="font-rubik text-[24px]/[1.2] font-black text-[#934541] uppercase">
+                  Jan Holan
+                </h3>
+                <p className="mt-[8px] text-[24px]/[1.2]">
+                  Minulý režim nezažil a poslední pobyt v rekreačním středisku
+                  absolvoval s traumatem a újmou na plyšovém psovi. Dlouhá léta
+                  se věnuje všem odstínům game designu, vystudoval tvorbu
+                  videoher a kreativně se jako člen Rollingu podílel například
+                  na larpu 97 Poets of Revachol.
+                </p>
+              </li>
+              <li>
+                <h3 className="font-rubik text-[24px]/[1.2] font-black text-[#934541] uppercase">
+                  Kamila Wagner
+                </h3>
+                <p className="mt-[8px] text-[24px]/[1.2]">
+                  Revoluci prožila jako buclaté batole a velmi rychle poté se
+                  stala fanouškem takových klenotů české kinematografie jako
+                  Dovolená s Andělem a Anděl na horách. Těší se, že díky larpu
+                  Sametová léta uvidí, jak by podobná dovolená probíhala
+                  doopravdy. Vedle vedení GenAI/ML projektů jí v poslední době
+                  život z(ne)příjemňuje právě tvorba a organizace larpů - 
+                  podílela se na 97 Poets of Revachol a redesignu Čistek.
+                </p>
+              </li>
+            </ul>
+            <ul className="flex flex-col gap-[24px] sm:grow-1 sm:basis-0 sm:gap-[36px]">
+              <li>
+                <h3 className="font-rubik text-[24px]/[1.2] font-black text-[#934541] uppercase">
+                  Michal Kunc
+                </h3>
+                <p className="mt-[8px] text-[24px]/[1.2]">
+                  Narodil se až ve federativním Československu, a tak přelomovou
+                  dobu 80. a 90. let zná pouze z vyprávění. Na podnikovou
+                  dovolenou by se ale někdy moc rád podíval, protože
+                  strukturovanou zábavu má ve velké oblibě. Ta ho ostatně
+                  zavedla i k larpům a Rollingu. Na 97 Poets of Revachol poprvé
+                  jako hráče, na Purges/Čistky jako organizátora
+                </p>
+              </li>
+              <li>
+                <h3 className="font-rubik text-[24px]/[1.2] font-black text-[#934541] uppercase">
+                  Adéla Kundrátová
+                </h3>
+                <p className="mt-[8px] text-[24px]/[1.2]">
+                  Patří ke generaci Z, takže zná sametovou revoluci pouze z
+                  barvitého vyprávění rodičů a zkresleného blábolení politiků.
+                  Má přehršel bláznivých nápadů, horkých úchopů a silných
+                  názorů. V reálném životě se věnuje sociologii, hudbě, boulderu
+                  a skotačení s přáteli.
+                </p>
+              </li>
+              <li>
+                <h3 className="font-rubik text-[24px]/[1.2] font-black text-[#934541] uppercase">
+                  Lucie Zelinka
+                </h3>
+                <p className="mt-[8px] text-[24px]/[1.2]">
+                  V přelomovém období život ji potkává mnoho poprvé, včetně
+                  prvního podílu na tvorbě larpu. V týmu může využívat toho, že
+                  s řadou dobových situací má významně bližší zkušenosti a může
+                  tak zúročit skutečnost, že její dospívání probíhalo v době,
+                  kdy se hra odehrává..
+                </p>
+              </li>
+            </ul>
+          </div>
+        </div>
       </main>
     </div>
   );
